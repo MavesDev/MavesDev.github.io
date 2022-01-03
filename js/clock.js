ae13d79276b629d05f1aeb575eb60a2b98e13504
MavesDev <Raqhinkusmanadinata@gmail.com>
2022-01-03 16:20:30 +0700

    Siap pakai

---
+++ js/clock.js
@@ -0,0 +1,75 @@
+let hours = () => {
+  let date = new Date();
+  let hrs = date.getHours();
+  let mins = date.getMinutes();
+  let secs = date.getSeconds();
+  let period = "AM";
+  if (hrs == 0) {
+    hrs = 12;
+  } else if (hrs >= 12) {
+    hrs = hrs - 12;
+    period = "PM";
+  }
+  hrs = hrs < 10 ? "0" + hrs : hrs;
+  mins = mins < 10 ? "0" + mins : mins;
+  secs = secs < 10 ? "0" + secs : secs;
+
+  let time = `${hrs}`;
+  document.getElementById("hours").innerText = time;
+  setTimeout(hours, 1000);
+};
+
+hours();
+
+// Minutes
+
+let mins = () => {
+  let date = new Date();
+  let hrs = date.getHours();
+  let mins = date.getMinutes();
+  let secs = date.getSeconds();
+  let period = "AM";
+  if (hrs == 0) {
+    hrs = 12;
+  } else if (hrs >= 12) {
+    hrs = hrs - 12;
+    period = "PM";
+  }
+  hrs = hrs < 10 ? "0" + hrs : hrs;
+  mins = mins < 10 ? "0" + mins : mins;
+  secs = secs < 10 ? "0" + secs : secs;
+
+  let time = `${mins}`;
+  document.getElementById("mins").innerText = time;
+  setTimeout(mins, 1000);
+};
+
+mins();
+
+// days
+
+let days = () => {
+  let date = new Date();
+  let hrs = date.getHours();
+  let mins = date.getMinutes();
+  let secs = date.getSeconds();
+  let period = "AM";
+  if (hrs == 0) {
+    hrs = 12;
+  } else if (hrs >= 12) {
+    hrs = hrs - 12;
+    period = "PM";
+  }
+  hrs = hrs < 10 ? "0" + hrs : hrs;
+  mins = mins < 10 ? "0" + mins : mins;
+  secs = secs < 10 ? "0" + secs : secs;
+
+  let time = `${period}`;
+  document.getElementById("days").innerText = time;
+  setTimeout(days, 1000);
+  console.log(date);
+};
+
+days();
+
+// Date
